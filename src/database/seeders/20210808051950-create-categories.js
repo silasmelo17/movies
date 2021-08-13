745a3f1ee09fb4401338a336b839c7ef91d17735
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-08-12 21:09:51 -0300

    update seed

--- src/database/seeders/20210808051950-create-categories.js
+++ src/database/seeders/20210808051950-create-categories.js
@@ -17,6 +17,6 @@ module.exports = {
     },
 
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.bulkDelete('People', null, {});
+        await queryInterface.bulkDelete('Categories', null, {});
     }
 };
